7c9837eb5c668448e12163e506e74bed376f5b24
jitu <jahirul.jitu7@gmail.com>
2022-12-03 00:44:35 +0600

    add

--- Asyncawait-2/script.js
+++ Asyncawait-2/script.js
@@ -4,10 +4,7 @@ const dataFunction = async ()=> {
     const datafetch = await fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
     const dataResponse = await datafetch.json()
     console.log(dataResponse.bpi.EUR.symbol);
-    // for(let dataa in dataResponse){
-    //     console.log(dataa.bpi.EUR);
-    // }
-    // console.log(dataResponse);
+
 }
 
 dataFunction()
